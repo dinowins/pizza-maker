fab875835ad214a26d573e94784c6b832658f979
Dino Nguyen <dinodnguyen@gmail.com>
2019-02-01 13:01:07 -0800

    Connect business logic to user interface, able to console.log pizza object

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+// business logic
+function Pizza() {
+  this.size = ""; //3 5 7 9
+  this.proteins = []; //$2 each
+  this.vegetables = []; //$1 each
+  this.cheese = []; //$1 each
+  this.sauce = [];
+  this.dough = "";
+  this.price = 0;
+}
+
+Pizza.prototype.getPriceForSize = function() {
+  if (this.size === "Small") {
+    this.price += 3;
+  } else if (this.size === "Medium") {
+    this.price += 5;
+  } else if (this.size === "Large") {
+    this.price += 7;
+  } else if (this.size === "Extra-Large") {
+    this.price += 9;
+  }
+}
+
+Pizza.prototype.getPriceForToppings = function() {
+  for (var i = 0; i < this.proteins.length; i++) {
+    if (this.proteins[i]) {
+      this.price += 2;
+    }
+  }
+  for (var i = 0; i < this.vegetables.length; i++) {
+    if (this.vegetables[i]) {
+      this.price += 1;
+    }
+  }
+  for (var i = 0; i < this.cheese.length; i++) {
+    if (this.cheese[i]) {
+      this.price += 1;
+    }
+  }
+}
+
+Pizza.prototype.getTotalPrice = function() {
+  return this.price;
+}
+
+// user interface
+$(function() {
+
+  var newPizza = new Pizza();
+
+  $("#pizza-maker").submit(function(event){
+    var pizzaSize = $("#pizza-size").val();
+    var dough = $("#pizza-dough").val();
+    var proteins = $("input:checkbox[name=proteins]:checked");
+    var vegetables = $("input:checkbox[name=vegetables]:checked");
+    var cheese = $("input:checkbox[name=cheese]:checked");
+    var sauce = $("input:checkbox[name=sauce]:checked");
+    newPizza.size = pizzaSize;
+    proteins.each(function() {
+      newPizza.proteins.push($(this).val());
+    });
+    vegetables.each(function() {
+      newPizza.vegetables.push($(this).val());
+    });
+    cheese.each(function() {
+      newPizza.cheese.push($(this).val());
+    });
+    sauce.each(function() {
+      newPizza.sauce.push($(this).val());
+    });
+    newPizza.dough = dough;
+    console.log(newPizza);
+    event.preventDefault();
+  });
+});
